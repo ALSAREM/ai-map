bd16d27be6b5dcc25f8e42ebbc59dbd9e521ae57
Mazen ALSAREM <ALSAREM@users.noreply.github.com>
2024-01-11 09:34:51 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# embed streamlit docs in a streamlit app
+components.iframe("https://maphub.net/embed_h/0AU3XS0pmQkJ3qu4?panel=1&legend=1&button=0",600, height=576, False)
